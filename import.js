13cb175276a0b40ca22bc06d347afaf6a62eaf77
Laura Zalite <laurazalite03@gmail.com>
2024-04-25 22:20:22 +0300

    Databse table creation if it doesn't exist

--- import.js
+++ import.js
@@ -1,4 +1,5 @@
 const fastCsv = require('fast-csv');
+const connectionString = 'postgres://localhost:password@hpostgres:5432/steam_data';
 
 //Function to read the csv file
 function readCsv(filePath) {
@@ -26,4 +27,22 @@ async function getReviewCounts(filePath) {
       return acc;
     }, {});
     return reviewCounts;
+  }
+
+  async function createTable(connection, tableName) {
+    const sql = `CREATE TABLE IF NOT EXISTS ${tableName} (
+      rank VARCHAR(255),
+      name VARCHAR(255),
+      platform VARCHAR(255),
+      year INT,
+      genre VARCHAR(255),
+      publisher VARCHAR(255),
+      NA_sales DECIMAL(10,2),
+      EU_sales DECIMAL(10,2),
+      JP_sales DECIMAL(10,2),
+      Other_sales DECIMAL(10,2),
+      Global_sales DECIMAL(10,2),
+      number_of_reviews INT
+    )`;
+    await connection.execute(sql);
   }
