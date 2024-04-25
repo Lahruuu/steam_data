70801c63185d49e53e52180f4bbf0bacd833cbf1
Laura Zalite <laurazalite03@gmail.com>
2024-04-25 22:03:45 +0300

    Added review counting function

--- import.js
+++ import.js
@@ -1,5 +1,6 @@
 const fastCsv = require('fast-csv');
 
+//Function to read the csv file
 function readCsv(filePath) {
     const data = [];
     return new Promise((resolve, reject) => {
@@ -11,6 +12,18 @@ function readCsv(filePath) {
     });
 }
 
+//Function that filters out any rows that are missing inforation
 function filterData(data) {
     return data.filter((row) => !Object.values(row).same((value) => !value));
 }
+
+//Function that counts each game's reviews
+async function getReviewCounts(filePath) {
+    const reviews = await readCsv(filePath);
+    const reviewCounts = reviews.reduce((acc, review) => {
+      const gameName = review.app_name;
+      acc[gameName] = (acc[gameName] || 0) + 1; // Count each occurrence of the game name
+      return acc;
+    }, {});
+    return reviewCounts;
+  }
